de77325b492fea6f4bb1169c8b1c51a83c932b55
Moinuddin Kamal <IT@pronayonbd.com>
2021-11-29 16:26:56 +0600

    portfolio site setup

---
+++ src/Pages/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h3>Thisi is Portfolio Section</h3>
+        </div>
+    );
+};
+
+export default Portfolio;
